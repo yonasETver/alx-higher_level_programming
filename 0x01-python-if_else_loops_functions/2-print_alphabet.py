facf9a0f847ea3b209bbe34700e21e4cac1bb280
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-06-28 16:13:36 +0300

    Create 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    print("{:s}".format(chr(i)), end="")
+i = ord('a')
+while i < ord('z') + 1:
+    print("{:c}".format(i), end='')
+    i+=1
