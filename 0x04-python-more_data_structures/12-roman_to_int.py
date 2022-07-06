041ea0435b56564574d33d4e2ba2509ed4a48f15
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-07-06 20:56:17 +0300

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,32 +1,29 @@
-#!/usr/bin/python3
-def roman_value(prmCharacter):
-    roman_list = [('I', 1), ('V', 5), ('X', 10),
-                  ('L', 50), ('C', 100), ('D', 500), ('M', 1000)]
-    for item in roman_list:
-        character, value = item
-        if (prmCharacter is character):
-            return value
-    return None
-
-def next_value(prmString, prmIndex):
-    if prmIndex + 1 < len(prmString):
-        return roman_value(prmString[prmIndex + 1])
-    else:
-        return None
+!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    result = 0
+    if roman_string != str(roman_string) or
+            roman_string is None):
+        return (0)
 
-    if (roman_string is None or isinstance(roman_string, str) is False):
-        return result
+    roman_dict = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+    num = 0
 
-    enum = enumerate(roman_string)
-    for index, character in enum:
-        currentValue = roman_value(character)
-        nextValue = next_value(roman_string, index)
-        if nextValue is None or currentValue >= nextValue:
-            result += currentValue
+    for j in range(len(roman_string)):
+        if roman_dict.get(roman_string[j], 0) == 0:
+            return (0)
+
+        if (j != (len(roman_string) - 1) and
+                roman_dict[roman_string[j]] < roman_dict[roman_string[j + 1]]):
+                num += roman_dict[roman_string[j]] * -1
+
         else:
-            result += (nextValue - currentValue)
-            next(enum)
-    return result
+            num += roman_dict[roman_string[j]]
+    return (num)
