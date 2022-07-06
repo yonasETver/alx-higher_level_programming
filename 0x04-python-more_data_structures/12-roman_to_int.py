db0df24a0a0a1e86ed81aada85b6a9b82d71380f
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-07-06 20:56:59 +0300

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,29 +1,15 @@
-!/usr/bin/python3
+#!/usr/bin/python3
+def roman_to_int(roman_string: str):
+    if roman_string is None or type(roman_string) != str:
+        return 0
+    data = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    numbers = [data[x] for x in roman_string] + [0]
+    rep = 0
 
-def roman_to_int(roman_string):
-    if roman_string != str(roman_string) or
-            roman_string is None):
-        return (0)
-
-    roman_dict = {
-        "I": 1,
-        "V": 5,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000
-    }
-    num = 0
-
-    for j in range(len(roman_string)):
-        if roman_dict.get(roman_string[j], 0) == 0:
-            return (0)
-
-        if (j != (len(roman_string) - 1) and
-                roman_dict[roman_string[j]] < roman_dict[roman_string[j + 1]]):
-                num += roman_dict[roman_string[j]] * -1
-
+    for i in range(len(numbers) - 1):
+        if numbers[i] >= numbers[i+1]:
+            rep += numbers[i]
         else:
-            num += roman_dict[roman_string[j]]
-    return (num)
+            rep -= numbers[i]
+
+    return rep
