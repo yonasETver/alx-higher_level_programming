ffe4b26b7d8a5f6e931ab9fb37afae5d89b5f6b6
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-07-21 19:16:50 +0300

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 """
 A function that adds 2 integers a and b .
-a and b must be integers or floats
-else raise type error exeption
 """
 
 def add_integer(a, b=98):
+    """
+    Add two integer 
+    Args: a - integer or float
+          b - integer or float
+    raise: - a and b must be integers or floats
+             else raise type error exeption
+    """
     if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
     if ((not isinstance(b, int) and not isinstance(b, float))):
