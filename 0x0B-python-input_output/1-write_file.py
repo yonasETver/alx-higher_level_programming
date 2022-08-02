5ce0b2ca99d3fcb281535960b7c54bd25c5a05ab
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-08-02 18:28:18 +0300

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,12 +3,13 @@
 Module for write_file method.
 """
 
+
 def write_file(filename="", text=""):
     """write_file writes a string to a text file.
     Args:
-        filename: name of file.
-        text: text to be written.
-    Return: the text.
+        filename (str): name of file.
+        text (str): text to be written.
+    Return: number of bytes written.
     """
     with open(filename, mode="w", encoding="UTF-8") as f:
         return (f.write(text))
