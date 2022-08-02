af7a655b6dce0d70a55ca09182722c15322eb6fb
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-08-02 18:30:58 +0300

    Update 2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """Defines a file-appending function."""
 
+
 def append_write(filename="", text=""):
     """Appends a string to the end of a UTF8 text file.
     Args:
-        filename: The name of the file to append to.
-        text: The string to append to the file.
+        filename (str): The name of the file to append to.
+        text (str): The string to append to the file.
     Returns:
         The number of characters appended.
     """
