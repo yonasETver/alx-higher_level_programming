8f2f6bce086581348aa58506293bfadded469379
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-08-02 18:21:21 +0300

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,6 +2,8 @@
 """
 function that writes a string to a text file (UTF8) and returns the number of characters written
 """
+
+
 def write_file(filename="", text=""):
     """ module write_file
     """
