4ed11cb6b971c10b98662b24a7e9d28fa2d47cb0
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-08-02 18:17:11 +0300

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,7 +5,9 @@ function that reads a text file from UTF8
 
 
 def read_file(filename=""):
-    """reads a text file (UTF8) and prints 
+    """
+    reads a text file (UTF8) and prints 
+    Returns: none
     """
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
