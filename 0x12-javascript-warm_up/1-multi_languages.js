8afe28dea7a79c575bf9015621b636d09c1de963
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-10-04 12:05:52 +0300

    Update 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-//prints 3 lines
+// It prints 3 lines
 
-console.log('C is fun\nPython is cool\nJavascript is amazing')
+console.log('C is fun\nPython is cool\nJavascript is amazing');
