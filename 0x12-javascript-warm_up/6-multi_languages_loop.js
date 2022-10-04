30d50162d3d22b4383d467b6f5de86ff01c8d90b
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-10-04 12:15:42 +0300

    Add files via upload

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const myStrArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+const arrayLength = myStrArray.length;
+for (let i = 0; i < arrayLength; i++) {
+  console.log(myStrArray[i]);
+}
