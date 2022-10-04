93d230ad41a9ef1413e3a3274d9cf973f94aac58
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-10-04 12:10:52 +0300

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/100-let_me_const.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/node
-// Modifies the value of myVar to 333
-
-myVar = 333;
--- 0x12-javascript-warm_up/3-value_argument.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-/* A script that prints the first 
-argument passed to it*/
-
-if (process.argv[2] === undefined) {
-  console.log('No argument');
-} else {
-  console.log(process.argv[2]);
-}
