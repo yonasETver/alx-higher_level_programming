39353265b93e75cf16004fe7fd727529b3c92aad
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-10-04 16:35:56 +0300

    Update 102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,26 +1,9 @@
 #!/usr/bin/node
-// concatenates two files
-const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', function (err, file) {
-  if (err) {
-    console.log(err);
-  } else {
-    fs.writeFile(process.argv[4], file, 'utf8', function (err) {
-      if (err) {
-        console.log(err);
-      }
-    });
-  }
-});
+'use strict';
 
-fs.readFile(process.argv[3], 'utf8', function (err, file) {
-  if (err) {
-    console.log(err);
-  } else {
-    fs.appendFile(process.argv[4], file, function (err) {
-      if (err) {
-        console.log(err);
-      }
-    });
-  }
+const fileSystem = require('fs');
+
+const firstArg = fileSystem.readFileSync(process.argv[2]).toString();
+const secondArg = fileSystem.readFileSync(process.argv[3]).toString();
+fileSystem.writeFileSync(process.argv[4], firstArg + secondArg);
