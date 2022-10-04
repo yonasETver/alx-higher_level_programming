03039b9a78ee636ea476e0c5b378783119fb875e
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-10-04 16:28:20 +0300

    Update 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
-// imports an array and computes a new array
+
 const list = require('./100-data').list;
 
-const factors = list.map(function (x, i) {
-  return x * i;
-});
-
 console.log(list);
-console.log(factors);
+console.log(list.map((x, i) => x * i));
