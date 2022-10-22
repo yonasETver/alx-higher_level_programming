b7d24576980e3d391e0fe21b0d3d5fcce0cc9524
yonasETver <101360122+yonasETver@users.noreply.github.com>
2022-10-22 10:54:45 +0300

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """
-fetches https://intranet.hbtn.io/status
+fetches https://alx-intranet.hbtn.io/status
 """
 if __name__ == "__main__":
     import urllib.request as request
-    with request.urlopen('https://intranet.hbtn.io/status') as r:
+    with request.urlopen('https://alx-intranet.hbtn.io/status') as r:
         html = r.read()
         print('Body response:')
         print("\t- type: {}".format(type(html)))
